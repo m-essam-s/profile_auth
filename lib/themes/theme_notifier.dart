50abcbd5a53d7b322529ad0bb1efdfecc83b454f
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-22 15:50:20 +0300

    themes Components

---
+++ lib/themes/theme_notifier.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:profile_auth/themes/theme.dart';
+
+class ThemeNotifier with ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  ThemeData get currentTheme => _isDarkMode ? darkTheme : lightTheme;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
